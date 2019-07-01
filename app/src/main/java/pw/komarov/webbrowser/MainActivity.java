c41b0451a9cf21ee18910ac81094f086a428c10f
Andrey Komarov (k0m@) <andrey@komarov.pw>
2019-07-01 03:43:33 +0300

    - Toolbar customized - More user-friendly interface & buttons

--- app/src/main/java/pw/komarov/webbrowser/MainActivity.java
+++ app/src/main/java/pw/komarov/webbrowser/MainActivity.java
@@ -1,24 +1,29 @@
 package pw.komarov.webbrowser;
 
 import android.graphics.Bitmap;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.SearchView;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
+public class MainActivity extends AppCompatActivity {
 
     private WebView webView;
     private ProgressBar progressBar;
-    private String currentUrl = "";
-    private String newUrl = "";
-    private MenuItem goStopRefreshItem;
+    private ImageButton btnGoStopRefresh;
+    private ImageButton btnBack;
+    private ImageButton btnForward;
+    private EditText edtUrl;
+    private boolean needUrlRefresh = false;
 
     private enum CurrentAction {GO, STOP, REFRESH};
     private CurrentAction currentAction = CurrentAction.GO;
@@ -27,16 +32,86 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
         this.currentAction = currentAction;
         switch (currentAction) {
             case GO:
-                goStopRefreshItem.setIcon(R.drawable.ic_go_black_24dp);
+                btnGoStopRefresh.setImageResource(R.drawable.ic_action_go_24dp);
                 break;
             case STOP:
-                goStopRefreshItem.setIcon(R.drawable.ic_stop_24dp);
+                btnGoStopRefresh.setImageResource(R.drawable.ic_action_stop_24dp);
                 break;
             case REFRESH:
-                goStopRefreshItem.setIcon(R.drawable.ic_refresh_24dp);
+                btnGoStopRefresh.setImageResource(R.drawable.ic_action_refresh_24dp);
                 break;
         }
 
+    }
+
+    private void initContent() {
+        webView = findViewById(R.id.webView);
+        progressBar = findViewById(R.id.pb);
+        btnGoStopRefresh = findViewById(R.id.btnGoStopRefresh);
+        edtUrl = findViewById(R.id.edtUrl);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Get the ActionBar here to configure the way it behaves.
+        final ActionBar ab = getSupportActionBar();
+        if(ab != null) {
+            ab.setDisplayShowHomeEnabled(false); // show or hide the default home button
+            ab.setDisplayHomeAsUpEnabled(false);
+            ab.setDisplayShowCustomEnabled(true); // enable overriding the default toolbar layout
+            ab.setDisplayShowTitleEnabled(false); // disable the default title element here (for centered title)
+        }
+
+        final EditText edtUrl = findViewById(R.id.edtUrl);
+
+        edtUrl.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                goUrl(v.getText().toString());
+                return true;
+            }
+        });
+        edtUrl.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                setCurrentAction(CurrentAction.GO);
+                return true;
+            }
+        });
+
+        ImageButton btnGoStopRefresh = findViewById(R.id.btnGoStopRefresh);
+        btnGoStopRefresh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (currentAction) {
+                    case GO:
+                        goUrl(edtUrl.getText().toString());
+                        break;
+                    case STOP:
+                        webView.stopLoading();
+                        break;
+                    case REFRESH:
+                        goUrl(edtUrl.getText().toString());
+                        break;
+                }
+            }
+        });
+
+        btnBack = findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goBack();
+            }
+        });
+
+        btnForward = findViewById(R.id.btnForward);
+        btnForward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goForward();
+            }
+        });
     }
 
     @Override
@@ -44,66 +119,14 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        webView = findViewById(R.id.webView);
-        progressBar = findViewById(R.id.pb);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-
-        MenuItem searchItem = menu.findItem(R.id.app_bar_search);
-        SearchView searchView = (SearchView) searchItem.getActionView();
-        searchView.setOnQueryTextListener(this);
-
-        goStopRefreshItem = menu.findItem(R.id.app_bar_go_stop_refresh);
-
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.app_bar_go_stop_refresh:
-                switch (currentAction) {
-                    case GO:
-                        getPage(newUrl);
-                        break;
-                    case STOP:
-                        webView.stopLoading();
-                        break;
-                    case REFRESH:
-                        getPage(currentUrl);
-                        break;
-                }
-                return true;
-            case R.id.app_bar_back:
-                goBack();
-                return true;
-            case R.id.app_bar_fwd:
-                goForward();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-
-    @Override
-    public boolean onQueryTextSubmit(String query) {
-        getPage(query);
-        return true;
-    }
-
-    @Override
-    public boolean onQueryTextChange(String newText) {
-        setCurrentAction(CurrentAction.GO);
-        newUrl = newText;
-        return true;
+        initContent();
     }
 
     public boolean goBack() {
         if(webView.canGoBack()) {
             webView.goBack();
+            needUrlRefresh = true;
+
             return true;
         } else
             return false;
@@ -112,6 +135,8 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
     public boolean goForward() {
         if(webView.canGoForward()) {
             webView.goForward();
+            needUrlRefresh = true;
+
             return true;
         } else
             return false;
@@ -123,9 +148,27 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
             super.onBackPressed();
     }
 
-    protected void getPage(String url) {
+    public void refreshNavigationState() {
+        if(needUrlRefresh) {
+            edtUrl.setText(webView.getUrl());
+            needUrlRefresh = false;
+        }
+
+        btnForward.setImageResource((webView.canGoForward())
+                ? R.drawable.ic_navigate_forward_24dp
+                    : R.drawable.ic_navigate_forward_disabled_24dp);
+
+        btnBack.setImageResource((webView.canGoBack())
+                ? R.drawable.ic_navigate_back_24dp
+                    : R.drawable.ic_navigate_back_disabled_24dp);
+    }
+
+    protected void goUrl(String url) {
+        if((!url.toLowerCase().startsWith("http://")) && (!url.toLowerCase().startsWith("https://")))
+            url = "https://" + url;
+        webView.requestFocus();
         setCurrentAction(CurrentAction.STOP);
-        currentUrl = url;
+
         webView.setWebViewClient(new WebViewClient() {
             @Override
             public void onPageStarted(WebView view, String url, Bitmap favicon) {
@@ -143,11 +186,11 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
                 if(newProgress == 100) {
                     progressBar.setVisibility(View.GONE);
                     setCurrentAction(CurrentAction.REFRESH);
+                    refreshNavigationState();
                 }
             }
         });
 
-        //webView.getSettings().setJavaScriptEnabled(true);
         webView.loadUrl(url);
     }
 }
